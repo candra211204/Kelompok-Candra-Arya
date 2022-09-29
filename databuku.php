c9faf63c920ccc5f8333115a9208ab2e99f2e75f
AryaPutra21-arch <onepiecenw12@gmail.com>
2022-09-29 13:58:53 +0700

    tghdg

--- databuku.php
+++ databuku.php
@@ -1,3 +1,24 @@
-<?php
 
+<?php
+session_start();
+include "config.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Buku Perpustakaan</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+</head>
+<body>
+    <div class="containerr ">
+        <form 
+
+
+    </div> 
+</body>
+</html>
